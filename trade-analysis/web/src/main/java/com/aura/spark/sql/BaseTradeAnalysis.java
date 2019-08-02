649a4847979d237b3d60b8c9e52c97fedd960c57
duanshihui <ly5423ems@163.com>
2019-08-02 17:57:08 +0800

    add userview model，update BaseTradeAnalysis

--- trade-analysis/web/src/main/java/com/aura/spark/sql/BaseTradeAnalysis.java
+++ trade-analysis/web/src/main/java/com/aura/spark/sql/BaseTradeAnalysis.java
@@ -2,6 +2,7 @@ package com.aura.spark.sql;
 
 import com.aura.model.ShopInfo;
 import com.aura.model.UserPay;
+import com.aura.model.UserView;
 import com.aura.util.StringUtil;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -76,6 +77,26 @@ abstract  public class BaseTradeAnalysis {
         System.out.println("UserPay Dataframe Schema");
         userPayDF.printSchema();
         return userPayDF;
+    }
+
+    protected JavaRDD<UserView> toUserViewRDD(String path) {
+        JavaRDD<String> rdd = jsc.textFile(path);
+        JavaRDD<UserView> userViewJavaRDD = rdd.map(line -> {
+            String[] parts = line.split(",",-1);
+            UserView bean = new UserView();
+            bean.setUser_id(parts[0]);
+            bean.setShop_id(parts[1]);
+            bean.setTime_stamp(parts[2]);
+            return bean;
+        });
+        return userViewJavaRDD;
+    }
+
+    protected Dataset<Row> toUserViewDF(JavaRDD<UserView> userViewJavaRDD) {
+        Dataset<Row> userViewDF = spark.createDataFrame(userViewJavaRDD,UserView.class);
+        System.out.println("UserView Dataframe Schema");
+        userViewDF.printSchema();
+        return userViewDF;
     }
 
 }
