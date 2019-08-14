1b524c0872ee540435291bc535dc6581fa87999c
MarshZe <17710559076@163.com>
2019-08-14 23:06:39 +0800

    Add files via upload
    修改查询结果从mysql获取

--- trade-analysis/web/src/main/java/com/aura/action/RetaindAnalysisAction.java
+++ trade-analysis/web/src/main/java/com/aura/action/RetaindAnalysisAction.java
@@ -3,6 +3,7 @@ package com.aura.action;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.aura.basic.BasicActionSupportImpl;
+import com.aura.database.JDBCUtils;
 import com.aura.presto.PrestoToJDBCClient;
 import com.aura.presto.RetainedAnalysis;
 import com.aura.util.JsonHelper;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,12 +32,13 @@ public class RetaindAnalysisAction extends BasicActionSupportImpl {
         String queryDate = this.getRequest().getParameter("startTime");
         //获取查询月份所有的日期
         System.out.println("--------调用-----"+queryDate);
-        List<String> dateList = prestoToJDBCClient.dayReport(queryDate);
+        //List<String> dateList = prestoToJDBCClient.dayReport(queryDate);
         List<Object> queryResult = new ArrayList<>();
-        JSONObject jsonObject = new JSONObject();
+        JSONObject jsonObject = null;
         Connection connection = null;
         Statement stmt = null;
-        try {
+        /*try {
+
             connection = prestoToJDBCClient.getConnect();
             stmt= connection.createStatement();
             for (String day: dateList
@@ -44,17 +47,41 @@ public class RetaindAnalysisAction extends BasicActionSupportImpl {
                 jsonObject = retainedAnalysis.CalculateRetention(day, stmt);
                 if (jsonObject != null){
                     queryResult.add(jsonObject);
+                    System.out.println("-----------save-----"+prestoToJDBCClient.svaeMysql(jsonObject));
                 }
             }
         }catch (Exception e){
             e.printStackTrace();
         }finally {
             prestoToJDBCClient.close(connection, null, stmt);
+        }*/
+        //从mysql查询结果
+        ResultSet rs = null;
+        try {
+            connection = JDBCUtils.getConnection();
+            stmt = connection.createStatement();
+            String sql = "select * from query_result where queryday like '"+queryDate+"%' order by queryday";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()){
+                jsonObject = new JSONObject();
+                jsonObject.put("day", rs.getString(1));
+                jsonObject.put("day0", rs.getString(2));
+                jsonObject.put("day1", rs.getString(3));
+                jsonObject.put("day2", rs.getString(4));
+                jsonObject.put("day3", rs.getString(5));
+                jsonObject.put("day4", rs.getString(6));
+                jsonObject.put("day5", rs.getString(7));
+                jsonObject.put("day6", rs.getString(8));
+                jsonObject.put("day7", rs.getString(9));
+                queryResult.add(jsonObject);
+                jsonObject = null;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            prestoToJDBCClient.close(connection,rs,stmt);
         }
-
-        System.out.println("JSONArray.toJSONString(queryResult)::::"+JSONArray.toJSONString(queryResult));
         JsonHelper.printBasicJsonList(getResponse(), queryResult);
-
     }
 
     public RetainedAnalysis getRetainedAnalysis() {
