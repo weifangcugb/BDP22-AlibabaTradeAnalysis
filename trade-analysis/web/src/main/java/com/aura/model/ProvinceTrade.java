4a80330c4cc69594f60eae2a75d67815a67d5781
weifang <weifang_cugb@163.com>
2019-08-16 22:50:01 +0800

    use aliyun mysql

--- trade-analysis/web/src/main/java/com/aura/model/ProvinceTrade.java
+++ trade-analysis/web/src/main/java/com/aura/model/ProvinceTrade.java
@@ -4,8 +4,16 @@ public class ProvinceTrade {
     String provinceName;
     Integer tradeCount;
 
+    public String getProvinceName() {
+        return provinceName;
+    }
+
     public void setProvinceName(String provinceName) {
         this.provinceName = provinceName;
+    }
+
+    public Integer getTradeCount() {
+        return tradeCount;
     }
 
     public void setTradeCount(Integer tradeCount) {
